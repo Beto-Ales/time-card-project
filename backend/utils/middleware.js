7a74568bc7a7aecf41e04573661bbdc890415416
Alberto Alesandroni <betoalesandroni@gmail.com>
2022-05-01 15:51:22 +0200

    stop using middleware

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -22,17 +22,17 @@ const errorHandler = (error, request, response, next) => {
     }
     next(error)
 }
-
-const calcSpecialHours = (request, response, next) => {
-    request.body.grid[0].row[0].totalNormal = request.body.grid[0].row[0].startWork * 2
-    request.body.grid[0].row[0].totalSpecial = request.body.grid[0].row[0].endWork * 3
-    console.log('startWork', request.body.grid[0].row[0].startWork, 'totalNormal', request.body.grid[0].row[0].totalNormal, 'totalSpecial', request.body.grid[0].row[0].totalSpecial);
-    next()
-}
+// this should be done from the frontend
+// const calcSpecialHours = (request, response, next) => {
+//     request.body.grid[0].row[0].totalNormal = request.body.grid[0].row[0].startWork * 2
+//     request.body.grid[0].row[0].totalSpecial = request.body.grid[0].row[0].endWork * 3
+//     console.log('startWork', request.body.grid[0].row[0].startWork, 'totalNormal', request.body.grid[0].row[0].totalNormal, 'totalSpecial', request.body.grid[0].row[0].totalSpecial);
+//     next()
+// }
 
 module.exports = {
     requestLogger,
     unknownEndpoint,
     errorHandler,
-    calcSpecialHours
+    // calcSpecialHours
 }
