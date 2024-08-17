96b2aa6e54db9d6f6e35cd41acae8027c694b5b9
Alberto Alesandroni <betoalesandroni@gmail.com>
2024-08-17 15:43:55 +0200

    nuevo componente para recuperar password
    es accesible sin estar logeado. se llega a traves de link enviado por e-mail que contiene token con id de usuario. llama al componente changePassword.

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -1,16 +1,24 @@
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+// components
+import ChangePasswordForm from './ChangePassword'
+// services
+import usersService from '../services/users'
 
 function ResetPassword() {
-  const [searchParams] = useSearchParams();
-  const token = searchParams.get('token');
+  const [searchParams] = useSearchParams()
+  const token = searchParams.get('token')
+
+  usersService.setToken(token)
 
   return (
     <div>
+      <Link to="/"><button className='screenBtn'>Login</button></Link>
       <h1>Reset Password</h1>
       <p>Token: {token}</p>
-      {/* You can now use the token to handle password reset logic */}
+      <ChangePasswordForm isRecoverPassword={true} />
     </div>
-  );
+  )
 }
 
-export default ResetPassword;
+export default ResetPassword
